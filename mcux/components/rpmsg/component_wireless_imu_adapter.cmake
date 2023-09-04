dcf312fba88696ac90412c1998b26e49b993596d
Fengming Ye <frank.ye@nxp.com>
2023-09-04 15:24:12 +0800

    mcux: components: rpmsg: add IMU task priority config
    Add IMU task priority config definition in cmake file.
    
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- mcux/components/rpmsg/component_wireless_imu_adapter.cmake
+++ mcux/components/rpmsg/component_wireless_imu_adapter.cmake
@@ -1,6 +1,8 @@
 include_guard()
 message("component_wireless_imu_adapter component is included.")
 
+zephyr_compile_definitions(IMU_TASK_PRIORITY=3)
+
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/fsl_adapter_rfimu.c
 )
