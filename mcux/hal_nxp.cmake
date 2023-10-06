d3786c6e551f07bf49382711efd5d8ebfa93081e
Declan Snyder <declan.snyder@nxp.com>
2023-10-06 11:37:22 -0500

    cmake: Enable build of GAU DAC
    Add cmake line to enable build of GAU DAC driver
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

--- mcux/hal_nxp.cmake
+++ mcux/hal_nxp.cmake
@@ -164,6 +164,7 @@ include_driver_ifdef(CONFIG_ADC_MCUX_ETC		adc_etc		driver_adc_etc)
 include_driver_ifdef(CONFIG_MCUX_XBARA			xbara		driver_xbara)
 include_driver_ifdef(CONFIG_QDEC_MCUX			enc		driver_enc)
 include_driver_ifdef(CONFIG_NXP_RF_IMU			imu		driver_imu)
+include_driver_ifdef(CONFIG_DAC_MCUX_GAU		cns_dac		driver_cns_dac)
 
 if ((${MCUX_DEVICE} MATCHES "MIMXRT1[0-9][0-9][0-9]") AND (NOT (CONFIG_SOC_MIMXRT1166_CM4 OR CONFIG_SOC_MIMXRT1176_CM4)))
   include_driver_ifdef(CONFIG_HAS_MCUX_CACHE		cache/armv7-m7	driver_cache_armv7_m7)
