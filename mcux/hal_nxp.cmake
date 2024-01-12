d4c0f7317990f4dbf87b8c9ac55e0371357f3c8b
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-01-12 23:42:37 +0000

    mcux: hal_nxp: add include directive for DMIC driver
    Add include directive for FSL dmic driver
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- mcux/hal_nxp.cmake
+++ mcux/hal_nxp.cmake
@@ -165,6 +165,7 @@ include_driver_ifdef(CONFIG_MCUX_XBARA			xbara		driver_xbara)
 include_driver_ifdef(CONFIG_QDEC_MCUX			enc		driver_enc)
 include_driver_ifdef(CONFIG_NXP_RF_IMU			imu		driver_imu)
 include_driver_ifdef(CONFIG_DAC_MCUX_GAU		cns_dac		driver_cns_dac)
+include_driver_ifdef(CONFIG_AUDIO_DMIC_MCUX		dmic		driver_dmic)
 
 if ((${MCUX_DEVICE} MATCHES "MIMXRT1[0-9][0-9][0-9]") AND (NOT (CONFIG_SOC_MIMXRT1166_CM4 OR CONFIG_SOC_MIMXRT1176_CM4)))
   include_driver_ifdef(CONFIG_HAS_MCUX_CACHE		cache/armv7-m7	driver_cache_armv7_m7)
