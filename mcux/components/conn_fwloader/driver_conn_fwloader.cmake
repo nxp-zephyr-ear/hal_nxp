671c96cbcef8c1be94f596a62abb97f144f1016f
Waqar Ali Tahir <waqar.tahir@nxp.com>
2024-02-06 16:55:01 +0100

    mcux: conn_fwloader: aligned with SDK-RFP3 source code
    conn_fw component is restructured to expose public and private apis.
    Hence, it can be used within tf-m loader service partition to provide
    services. Apps without tfm can keep using it via the public apis.
    Moreover, during alignment of code, few fixes have been made
    to enable build  with Zephyr.
    Commit also includes the cmake update of conn_fw(currently
    used for zephyr) path to be not used with tfm as its a copied
    version of conn_fw which is to be removed and conn_fw version
    of hal_nxp release path shall be used in future.
    
    Signed-off-by: Waqar Ali Tahir <waqar.tahir@nxp.com>

--- mcux/components/conn_fwloader/driver_conn_fwloader.cmake
+++ mcux/components/conn_fwloader/driver_conn_fwloader.cmake
@@ -1,6 +1,9 @@
 include_guard()
 message("driver_conn_fwloader component is included.")
 
+# To be migrated to use the hal_nxp release path of conn_fwloader
+if(NOT CONFIG_BUILD_WITH_TFM)
+
 target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/fsl_loader.c
     ${CMAKE_CURRENT_LIST_DIR}/nboot_hal.c
@@ -10,3 +13,4 @@ target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
     ${CMAKE_CURRENT_LIST_DIR}/include
 )
+endif()
