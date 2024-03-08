fa48ffeeaf8adcb4455e07b00c019335f48ff53d
Gang Li <gang.li_1@nxp.com>
2024-03-08 19:15:54 +0900

    zephyr/rw61x: Support monolithic build for combo firmware
    Extend monolithic build to support combo firmware.
    Replace CONFIG_NXP_FW_LOADER_MONOLITHIC with CONFIG_MONOLITHIC_WIFI/
    CONFIG_MONOLITHIC_BLE/CONFIG_MONOLITHIC_IEEE802154.
    
    Signed-off-by: Gang Li <gang.li_1@nxp.com>

--- zephyr/src/rw61x/rw61x_cpu1.c
+++ zephyr/src/rw61x/rw61x_cpu1.c
@@ -9,6 +9,7 @@
 
 #include <stdint.h>
 
+#if defined(CONFIG_MONOLITHIC_WIFI)
 __attribute__ ((__section__(".fw_cpu1"), used))
 const uint8_t fw_cpu1[] = {
     #include <rw61x_wifi_fw.bin.inc>
@@ -16,3 +17,4 @@ const uint8_t fw_cpu1[] = {
 
 const unsigned char *wlan_fw_bin = (const unsigned char *)(void *)&fw_cpu1[0];
 const unsigned int wlan_fw_bin_len = sizeof(fw_cpu1);
+#endif
--- zephyr/src/rw61x/rw61x_cpu2.c
+++ zephyr/src/rw61x/rw61x_cpu2.c
@@ -9,8 +9,14 @@
 
 #include <stdint.h>
 
+#if defined(CONFIG_MONOLITHIC_IEEE802154)
+__attribute__ ((__section__(".fw_cpu2_combo"), used))
+const uint8_t fw_cpu2_combo[] = {
+    #include <rw61x_combo_fw.bin.inc>
+};
+#elif defined(CONFIG_MONOLITHIC_BT)
 __attribute__ ((__section__(".fw_cpu2_ble"), used))
 const uint8_t fw_cpu2_ble[] = {
     #include <rw61x_ble_fw.bin.inc>
 };
-
+#endif
